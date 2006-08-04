7aae7fac9c565199ae0aa1eca3c94a7916e69569
arg@10ksloc.org <unknown>
2006-08-04 10:32:15 +0200

    removed config.h

--- config.h
+++
@@ -1,9 +0,0 @@
-/*
- * (C)opyright MMVI Anselm R. Garbe <garbeam at gmail dot com>
- * See LICENSE file for license details.
- */
-
-#define FONT			"-*-terminus-medium-*-*-*-13-*-*-*-*-*-iso10646-*"
-#define BGCOLOR			"#0a2c2d"
-#define FGCOLOR			"#ddeeee"
-#define BORDERCOLOR		"#176164"
