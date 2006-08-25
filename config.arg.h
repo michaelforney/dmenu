11bda99968c6c755065b35605d5c00846db0665f
Anselm R. Garbe <arg@10kloc.org>
2006-08-25 18:15:24 +0200

    fixed

--- config.arg.h
+++ config.arg.h
@@ -3,7 +3,7 @@
  * See LICENSE file for license details.
  */
 
-#define FONT			"-*-terminus-medium-*-*-*-14-*-*-*-*-*-iso10646-*"
+#define FONT			"-*-terminus-medium-*-*-*-12-*-*-*-*-*-iso10646-*"
 #define SELBGCOLOR		"#333366"
 #define SELFGCOLOR		"#eeeeee"
 #define NORMBGCOLOR		"#333333"
