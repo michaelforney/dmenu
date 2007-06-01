07239bbddd22a6333495e3fab4e0a1e4878b0ca9
Anselm R. Garbe <arg@suckless.org>
2007-06-01 12:28:30 +0200

    foooooo

--- dmenu.h
+++ dmenu.h
@@ -26,9 +26,9 @@ typedef struct {
 	} font;
 } DC; /* draw context */
 
-int screen;
-Display *dpy;
-DC dc;			/* global drawing context */
+extern int screen;
+extern Display *dpy;
+extern DC dc;				/* global drawing context */
 
 /* draw.c */
 void drawtext(const char *text, unsigned long col[ColLast]);
@@ -36,6 +36,6 @@ unsigned int textw(const char *text);
 unsigned int textnw(const char *text, unsigned int len);
 
 /* util.c */
-void *emalloc(unsigned int size);		/* allocates memory, exits on error */
-void eprint(const char *errstr, ...);		/* prints errstr and exits with 1 */
-char *estrdup(const char *str);			/* duplicates str, exits on allocation error */
+void *emalloc(unsigned int size);	/* allocates memory, exits on error */
+void eprint(const char *errstr, ...);	/* prints errstr and exits with 1 */
+char *estrdup(const char *str);		/* duplicates str, exits on allocation error */
