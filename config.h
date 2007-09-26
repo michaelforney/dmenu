6674bac1d3d0e1180fc4f9ea114f574eeb20f263
Anselm R. Garbe <garbeam@gmail.com>
2007-09-26 19:15:47 +0200

    applied my favorite color scheme

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-#define FONT			"-*-terminus-medium-r-*-*-12-*-*-*-*-*-iso10646-*"
-#define NORMBGCOLOR		"#ffffff"
-#define NORMFGCOLOR		"#000000"
-#define SELBGCOLOR		"#0000ff"
-#define SELFGCOLOR		"#ffffff"
+#define FONT			"-*-proggyclean-*-*-*-*-*-*-*-*-*-*-*-*"
+#define NORMBGCOLOR             "#ffffff"
+#define NORMFGCOLOR             "#333333"
+#define SELBGCOLOR              "#3366ff"
+#define SELFGCOLOR              "#ffffff"
 /* next macro defines the space between menu items */
 #define SPACE			30 /* px */
