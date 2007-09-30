fdc1dba7ce832797953807bbbb17e13f4857298a
Anselm R. Garbe <garbeam@gmail.com>
2007-09-30 12:47:40 +0200

    using saner defaults

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-#define FONT			"-*-proggyclean-*-*-*-*-*-*-*-*-*-*-*-*"
-#define NORMBGCOLOR             "#dddddd"
-#define NORMFGCOLOR             "#333333"
-#define SELBGCOLOR              "#0066cc"
+#define FONT			"-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*"
+#define NORMBGCOLOR             "#cccccc"
+#define NORMFGCOLOR             "#000000"
+#define SELBGCOLOR              "#0066ff"
 #define SELFGCOLOR              "#ffffff"
 /* next macro defines the space between menu items */
 #define SPACE			30 /* px */
