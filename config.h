1c488e6dac6e327ce06685556b1989bd75faf241
Anselm R. Garbe <garbeam@gmail.com>
2007-10-01 11:44:25 +0200

    fixed font definition

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-#define FONT			"-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*"
+#define FONT			"-*-terminus-medium-*-*-*-*-*-*-*-*-*-*-*"
 #define NORMBGCOLOR             "#cccccc"
 #define NORMFGCOLOR             "#000000"
 #define SELBGCOLOR              "#0066ff"
