4fc6cbe6086ed940c9a8eacc9ba8c2405c3f96ac
Anselm R Garbe <garbeam@gmail.com>
2008-06-13 11:53:53 +0100

    yet another cosmetic change

--- config.h
+++ config.h
@@ -1,10 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-#define FONT			"-*-terminus-medium-r-normal-*-14-*-*-*-*-*-*-*"
-#define NORMBGCOLOR             "#cccccc"
-#define NORMFGCOLOR             "#000000"
-#define SELBGCOLOR              "#0066ff"
-#define SELFGCOLOR              "#ffffff"
+#define FONT          "-*-terminus-medium-r-normal-*-14-*-*-*-*-*-*-*"
+#define NORMBGCOLOR   "#cccccc"
+#define NORMFGCOLOR   "#000000"
+#define SELBGCOLOR    "#0066ff"
+#define SELFGCOLOR    "#ffffff"
 
-static uint spaceitem           = 30; /* px between menu items */
+static uint spaceitem = 30; /* px between menu items */
+
+#ifdef XINERAMA
+static uint xidx      = 0;        /* Xinerama screen index to use */
+#endif
