8cc28cb4265b0c088ea909e58f42342b2214722e
Connor Lane Smith <cls@lubutu.com>
2011-11-27 23:35:09 +0100

    simplify stest

--- stest.c
+++ stest.c
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <dirent.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,80 +7,73 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-#define OPER(x)  (oper[(x)-'a'])
+#define FLAG(x)  (flag[(x)-'a'])
 
-static bool test(const char *);
+static void test(const char *, const char *);
 
-static bool quiet = false;
-static bool oper[26];
+static bool match = false;
+static bool flag[26];
 static struct stat old, new;
 
 int
 main(int argc, char *argv[]) {
-	char buf[BUFSIZ], *p;
-	bool match = false;
+	struct dirent *d;
+	char buf[BUFSIZ];
+	DIR *dir;
 	int opt;
 
-	while((opt = getopt(argc, argv, "C:bcdefghn:o:pqrsuwx")) != -1)
+	while((opt = getopt(argc, argv, "abcdefghln:o:pqrsuwx")) != -1)
 		switch(opt) {
-		case 'C': /* tests relative to directory */
-			if(chdir(optarg) == -1) {
-				perror(optarg);
-				exit(2);
-			}
-			break;
 		case 'n': /* newer than file */
 		case 'o': /* older than file */
-			if(!(OPER(opt) = stat(optarg, (opt == 'n' ? &new : &old)) == 0))
+			if(!(FLAG(opt) = !stat(optarg, (opt == 'n' ? &new : &old))))
 				perror(optarg);
 			break;
-		case 'q': /* quiet (no output, just status) */
-			quiet = true;
-			break;
 		default:  /* miscellaneous operators */
-			OPER(opt) = true;
+			FLAG(opt) = true;
 			break;
 		case '?': /* error: unknown flag */
-			fprintf(stderr, "usage: %s [-bcdefghpqrsuwx] [-C dir] [-n file] [-o file] [file...]\n", argv[0]);
+			fprintf(stderr, "usage: %s [-abcdefghlpqrsuwx] [-n file] [-o file] [file...]\n", argv[0]);
 			exit(2);
 		}
-	if(optind == argc)
-		while(fgets(buf, sizeof buf, stdin)) {
-			if(*(p = &buf[strlen(buf)-1]) == '\n')
-				*p = '\0';
-			match |= test(buf);
+	for(; optind < argc; optind++)
+		if(FLAG('l') && (dir = opendir(argv[optind]))) {
+			/* test directory contents */
+			while((d = readdir(dir)))
+				if(snprintf(buf, sizeof buf, "%s/%s", argv[optind], d->d_name) < sizeof buf)
+					test(buf, d->d_name);
+			closedir(dir);
 		}
-	else
-		while(optind < argc)
-			match |= test(argv[optind++]);
+		else
+			test(argv[optind], argv[optind]);
 
 	return match ? 0 : 1;
 }
 
-bool
-test(const char *path) {
-	struct stat st;
+void
+test(const char *path, const char *name) {
+	struct stat st, ln;
 
-	if((!OPER('b') || (stat(path, &st) == 0 && S_ISBLK(st.st_mode)))        /* block special     */
-	&& (!OPER('c') || (stat(path, &st) == 0 && S_ISCHR(st.st_mode)))        /* character special */
-	&& (!OPER('d') || (stat(path, &st) == 0 && S_ISDIR(st.st_mode)))        /* directory         */
-	&& (!OPER('e') || (access(path, F_OK) == 0))                            /* exists            */
-	&& (!OPER('f') || (stat(path, &st) == 0 && S_ISREG(st.st_mode)))        /* regular file      */
-	&& (!OPER('g') || (stat(path, &st) == 0 && (st.st_mode & S_ISGID)))     /* set-group-id flag */
-	&& (!OPER('h') || (lstat(path, &st) == 0 && S_ISLNK(st.st_mode)))       /* symbolic link     */
-	&& (!OPER('n') || (stat(path, &st) == 0 && st.st_mtime > new.st_mtime)) /* newer than file   */
-	&& (!OPER('o') || (stat(path, &st) == 0 && st.st_mtime < old.st_mtime)) /* older than file   */
-	&& (!OPER('p') || (stat(path, &st) == 0 && S_ISFIFO(st.st_mode)))       /* named pipe        */
-	&& (!OPER('r') || (access(path, R_OK) == 0))                            /* readable          */
-	&& (!OPER('s') || (stat(path, &st) == 0 && st.st_size > 0))             /* not empty         */
-	&& (!OPER('u') || (stat(path, &st) == 0 && (st.st_mode & S_ISUID)))     /* set-user-id flag  */
-	&& (!OPER('w') || (access(path, W_OK) == 0))                            /* writable          */
-	&& (!OPER('x') || (access(path, X_OK) == 0))) {                         /* executable        */
-		if(quiet)
+	if(!stat(path, &st) && !lstat(path, &ln)
+	&& ( FLAG('a') || name[0] != '.')                             /* hidden            */
+	&& (!FLAG('b') || S_ISBLK(st.st_mode))                        /* block special     */
+	&& (!FLAG('c') || S_ISCHR(st.st_mode))                        /* character special */
+	&& (!FLAG('d') || S_ISDIR(st.st_mode))                        /* directory         */
+	&& (!FLAG('e') || access(path, F_OK) == 0)                    /* exists            */
+	&& (!FLAG('f') || S_ISREG(st.st_mode))                        /* regular file      */
+	&& (!FLAG('g') || st.st_mode & S_ISGID)                       /* set-group-id flag */
+	&& (!FLAG('h') || (!lstat(path, &ln) && S_ISLNK(ln.st_mode))) /* symbolic link     */
+	&& (!FLAG('n') || st.st_mtime > new.st_mtime)                 /* newer than file   */
+	&& (!FLAG('o') || st.st_mtime < old.st_mtime)                 /* older than file   */
+	&& (!FLAG('p') || S_ISFIFO(st.st_mode))                       /* named pipe        */
+	&& (!FLAG('r') || access(path, R_OK) == 0)                    /* readable          */
+	&& (!FLAG('s') || st.st_size > 0)                             /* not empty         */
+	&& (!FLAG('u') || st.st_mode & S_ISUID)                       /* set-user-id flag  */
+	&& (!FLAG('w') || access(path, W_OK) == 0)                    /* writable          */
+	&& (!FLAG('x') || access(path, X_OK) == 0)) {                 /* executable        */
+		if(FLAG('q'))
 			exit(0);
-		puts(path);
-		return true;
+		match = true;
+		puts(name);
 	}
-	else
-		return false;
 }
