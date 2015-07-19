5feb0c689dd815a31d48e2007b364057f6fec773
Hiltjo Posthuma <hiltjo@codemadness.org>
2015-07-19 20:32:08 +0200

    config.mk: add FREETYPELIBS and FREETYPEINC
    These variables make it simpler to change the paths to this for ports.
    `pkg-config` is avoided because it sucks, in particular for cross-compilation.
    
    A commented path for *BSD is added, the Xft includes are located at:
    /usr/X11R6/include there.
    
    Also already bump the version number to 4.6, a release will come
    approximately in August.

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # dmenu version
-VERSION = 4.5-tip
+VERSION = 4.6
 
 # paths
 PREFIX = /usr/local
@@ -12,9 +12,15 @@ X11LIB = /usr/X11R6/lib
 XINERAMALIBS  = -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = ${X11INC}/freetype2
+
 # includes and libs
-INCS = -I${X11INC} -I/usr/include/freetype2
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} -lfontconfig -lXft
+INCS = -I${X11INC} -I${FREETYPEINC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
