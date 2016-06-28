3c91eed0fb74657c3fa25bc4fd65cd0aa88464c0
Hiltjo Posthuma <hiltjo@codemadness.org>
2016-06-28 17:56:25 +0200

    config.def.h: style improvement, use color Scheme enum

--- config.def.h
+++ config.def.h
@@ -7,11 +7,11 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[][2] = {
-/*     fg         bg       */
-	{ "#bbbbbb", "#222222" }, /* normal */
-	{ "#eeeeee", "#005577" }, /* selected */
-	{ "#000000", "#00ffff" }, /* out */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
